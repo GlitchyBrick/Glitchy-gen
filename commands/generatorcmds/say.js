1c781b33a34d5c5cb86ba60314ae98b54c15c9f8
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-06-19 20:10:41 +0100

    Create say.js

---
+++ commands/generatorcmds/say.js
@@ -0,0 +1,32 @@
+const { Command } = require("discord.js-commando");
+const Discord = require("discord.js");
+
+module.exports = class SayCommand extends Command {
+  // Change it to "SayCommand" instad of "GenCommand"
+  constructor(client) {
+    super(client, {
+      name: "say",
+      group: "generatorcmds",
+      memberName: "say",
+      guildOnly: true,
+      description: "test",
+      args: [
+        {
+          key: "msg",
+          prompt: "Please Add A MSG.",
+          type: "string"
+          //validate: givenLink
+        }
+      ]
+    });
+  }
+
+  run(message, { msg }) {
+    message.delete();
+    if (message.member.hasPermission(["ADMINISTRATOR"])) {
+      return message.say(msg);
+    } else {
+      message.say('You Are Missing The "ADMINISTRATOR" Permission.');
+    }
+  }
+};
