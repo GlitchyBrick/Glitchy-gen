d90b98111fffac70dab96f86afe3305b6078363a
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-06-19 20:14:59 +0100

    Create giveaway-reroll.js

---
+++ commands/admin/giveaway-reroll.js
@@ -0,0 +1,43 @@
+let fs = require("fs");
+const { Command } = require("discord.js-commando");
+const Discord = require("discord.js");
+const { Client, MessageAttachment } = require("discord.js");
+const messages = JSON.parse(fs.readFileSync("./messages.json"))
+const settings = JSON.parse(fs.readFileSync("./settings.json"))
+const giveawaymanager = require("/app/utils/giveawaymanager.js");
+const index = require('/app/index.js')
+
+module.exports = class DMCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "greroll",
+      group: "admin",
+      memberName: "greroll",
+      guildOnly: true,
+      description: "test",
+      throttling: {
+        usages: 1,
+        duration: 1
+      },
+      args: [
+        {
+          key: 'msgid',
+          prompt: 'MessageID Of Giveaway.',
+          type: 'string'
+        }
+      ]
+    });
+  }
+  
+  run(message, { msgid }) {
+    
+    message.delete()
+    
+    if (!message.member.hasPermission(["ADMINISTRATOR"])) { return; }
+    if(settings.Enabled==false){
+      message.say(messages.ServicesDown)
+      return;
+    }
+    giveawaymanager.reroll(msgid,message.channel)
+  }
+};
