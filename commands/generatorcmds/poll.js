1f5706be5e688d8ef477ee42291b67aa683a9251
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-06-19 20:09:37 +0100

    Create poll.js

---
+++ commands/generatorcmds/poll.js
@@ -0,0 +1,48 @@
+let fs = require("fs");
+const { Command } = require("discord.js-commando");
+const Discord = require("discord.js");
+const UsedByRecently = new Set();
+
+module.exports = class VoteCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "poll",
+      group: "generatorcmds",
+      memberName: "poll",
+      guildOnly: false,
+      description: "test"
+    });
+  }
+
+ run(message) {
+       if (message.member.hasPermission(["ADMINISTRATOR"])) {
+
+    const args = message.content.slice(";").split(" ");
+    const command = args.shift().toLowerCase();
+    
+    if(!args[0]){
+      return message.reply("You Cant Start A Vote Without A Text. So Add One!")
+    }
+
+    var argz = "";
+
+    for (var arg in args) {
+      var argz = argz + args[arg] + " ";
+    }
+
+    if (argz) {
+      const exampleEmbed = new Discord.MessageEmbed();
+        exampleEmbed.setColor("#ff1100");
+        exampleEmbed.setTitle("**POLL**");
+        exampleEmbed.setDescription(argz)
+        exampleEmbed.setFooter("Poll By " + message.member.user.tag)
+      
+        message.channel.send("@here\n", exampleEmbed).then(sentEmbed => {
+          sentEmbed.react("✅");
+          sentEmbed.react("❌");
+        });
+        message.delete();
+        }
+       }else message.say("You Need the Administrator Perm")
+  }
+};
