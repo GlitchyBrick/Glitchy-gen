eebe9b85d5cb149552071ad31f4992f476e746db
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-06-19 20:04:42 +0100

    Create embedoptions.js

---
+++ commands/generatorcmds/embedoptions.js
@@ -0,0 +1,30 @@
+const { Command } = require("discord.js-commando");
+const Discord = require("discord.js");
+
+module.exports = class SayCommand extends Command {
+  // Change it to "SayCommand" instad of "GenCommand"
+  constructor(client) {
+    super(client, {
+      name: "embedoptions",
+      group: "generatorcmds",
+      memberName: "embedoptions",
+      guildOnly: true,
+      description: "test",
+      
+      
+    });
+  }
+
+  run(message) {
+  
+    const exampleEmbed = new Discord.MessageEmbed()
+      .setColor("#00fa11")
+      .setTitle("Colour Options For SayEmbed")
+      .setDescription("`AQUA`, `GREEN`, `BLUE`, `PURPLE`, `GOLD`, `ORANGE`, `RED`, `GREY`, `DARKER_GREY`, `NAVY`, `DARK_AQUA`, `DARK_GREEN`, `DARK_BLUE`, `DARK_PURPLE`, `DARK_GOLD`, `DARK_ORANGE`, `DARK_RED`, `DARK_GREY`, `LIGHT_GREY`, `DARK_NAVY`, `LUMINOUS_VIVID_PINK`, `DARK_VIVID_PINK`")
+      
+  
+ 
+    message.say(exampleEmbed);
+    
+  }
+};
