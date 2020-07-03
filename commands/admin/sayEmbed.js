57a87a3a3629b45a731b8b1f13745a66b88eebf9
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-07-03 03:03:06 +0100

    Update sayEmbed.js

--- commands/admin/sayEmbed.js
+++ commands/admin/sayEmbed.js
@@ -6,7 +6,7 @@ module.exports = class SayCommand extends Command {
   constructor(client) {
     super(client, {
       name: "sayembed",
-      group: "generatorcmds",
+      group: "admin",
       memberName: "sayembed",
       guildOnly: true,
       description: "test",
