9fe46195410ac0fc82dae7754b606ebd53d72ca7
GlitchyBrick <67028881+GlitchyBrick@users.noreply.github.com>
2020-07-03 02:55:56 +0100

    Update help.js

--- commands/member/help.js
+++ commands/member/help.js
@@ -5,15 +5,12 @@ const { Client, MessageAttachment } = require("discord.js");
 const messages = JSON.parse(fs.readFileSync("./messages.json"));
 const settings = JSON.parse(fs.readFileSync("./settings.json"));
 const blockedusers = JSON.parse(fs.readFileSync("./blockedusers.json"));
-const mentionHook = new Discord.WebhookClient(
-  "713022747420262716",
-  "l1LSZO4yaDfR1re9SfMnne8Uw4Bvk3a4Q1G4Y5l7aGK50swletcUBMHjGV_yjggLXI1O"
-);
+
 module.exports = class HelpCommand extends Command {
   constructor(client) {
     super(client, {
       name: "help",
-      group: "generatorcmds",
+      group: "member",
       memberName: "help",
       guildOnly: true,
       description: "test"
@@ -24,7 +21,7 @@ module.exports = class HelpCommand extends Command {
     const exampleEmbed = new Discord.MessageEmbed()
       .setColor("#0099ff")
       .setTitle("Help Menu:")
-      .setDescription("**Generator Help**\n`=Gens`\n`=stock`\n`=captcha`\n`=trivia`\n\n**Mod Help**\n`=gstart, =greroll, =gdelete`\n`=drop`\n`=sayembed, =embedoptions`\n`=say`\n`=poll`\n\n**General Help**\n`=info`\n`=contact`\n`=invite`\n`=port`\n`=report`\n`=status`")
+      .setDescription("**Generator Help**\n`=Gens`\n`=stock`\n`=captcha`\n`=trivia`\n\n**Mod Help**\n`=gstart, =greroll, =gdelete`\n`=drop`\n`=sayembed, =embedoptions`\n`=say`\n`=poll`\n\n**General Help**\n`=info`\n`=contact`\n`=invite`\n`=port`\n`=report`\n`=suggest`\n`=status`\n`=vouch`")
  
     message.say(exampleEmbed);
   }
